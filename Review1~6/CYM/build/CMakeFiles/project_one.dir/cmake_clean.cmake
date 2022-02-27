274f9d657b1a51ccf83604c4ad7142b6a3afb6a9
Kevin <eagle9706@naver.com>
2022-02-27 23:38:45 +0900

    1~6 review done

---
+++ Review1~6/CYM/build/CMakeFiles/project_one.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_one.dir/main.cpp.o"
+  "project_one"
+  "project_one.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_one.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
